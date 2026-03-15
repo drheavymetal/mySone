a40cc4c63199ce610ce5f0e42cb0c2dadefb4b00
lullabyX <hassan_rabbi@live.com>
2026-03-16 03:03:02 +0600

    fix: disable context menu, selection, and image drag in miniplayer

--- src/miniplayer-main.tsx
+++ src/miniplayer-main.tsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import "./miniplayer.css";
 import MiniPlayer from "./components/MiniPlayer";
 
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <MiniPlayer />,
 );
